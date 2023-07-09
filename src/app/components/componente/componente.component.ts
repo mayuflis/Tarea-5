6f3eba53bc8fb43216ef2a61f7644b258cfb47a9
SergioDev <mayitavista@gmail.com>
2023-07-09 19:01:10 +0200

    modified:   src/app/app.component.css 	modified:   src/app/components/componente/componente.component.html 	modified:   src/app/components/componente/componente.component.ts 	modified:   src/styles.css

--- src/app/components/componente/componente.component.ts
+++ src/app/components/componente/componente.component.ts
@@ -3,65 +3,67 @@ import { Noticias } from 'src/app/interfaces/noticias.interface';
 @Component({
   selector: 'app-componente',
   templateUrl: './componente.component.html',
-  styleUrls: ['./componente.component.css']
+  styleUrls: ['./componente.component.css'],
 })
 export class ComponenteComponent {
-
-arrayNoticias:Noticias[]=[];
-newNoticia:Noticias ;
-fecha:Date=new Date("1/1/1900");
-  constructor(){
-   this.newNoticia={
-    title:"",
-    image:"",
-    body:"",
-    date: this.fecha
-   }
-    this.arrayNoticias=[
+  arrayNoticias: Noticias[] = [];
+  newNoticia: Noticias;
+  miClase: string = 'seccionNoticias__cards';
+  fecha: Date = new Date('1/1/1900');
+  constructor() {
+    this.newNoticia = {
+      title: '',
+      image: '',
+      body: '',
+      date: this.fecha,
+    };
+    this.arrayNoticias = [
       {
-        title:"Españoles por el mundo",
-        image:"http://via.placeholder.com/200x200",
-        body:"jkfljkdfjkasljkljdfsld",
-        date: new Date("1/12/2022")
+        title: 'Españoles por el mundo',
+        image: 'http://via.placeholder.com/200x200',
+        body: 'jkfljkdfjkasljkljdfsld',
+        date: new Date('1/12/2022'),
       },
       {
-        title:"Guerra de Ucrania",
-        image:"http://via.placeholder.com/200x200",
-        body:"jkfljkdfjkasljkljdfsld",
-        date: new Date("2/05/2023")
-      }]
-    
+        title: 'Guerra de Ucrania',
+        image: 'http://via.placeholder.com/200x200',
+        body: 'jkfljkdfjkasljkljdfsld',
+        date: new Date('2/05/2023'),
+      },
+    ];
   }
 
-  mostrarinfo():string{
-
-    let lista=""
-    this.arrayNoticias.forEach(noticias=>{
-      lista+=`<div class="seccionNoticias__cards">
+  mostrarinfo(): string {
+    let lista = '';
+    this.arrayNoticias.forEach((noticias) => {
+      lista += `<div class="seccionNoticias__cards">
         <h2>${noticias.title}</h2>
         <img src="${noticias.image}"class="imagen">
-        <p class="cuerpo">${noticias.body}</p>
-        <p>Fecha: ${noticias.date}</p>
-        </div>`
-    })
+        <p class="parrafo">${noticias.body}</p>
+        <p class="parrafo">Fecha: ${noticias.date}</p>
+        </div>`;
+    });
     return lista;
   }
 
-  guardarNoticia(){
-
-    let fecha= new Date("1/1/1900").getFullYear();
-    if(this.newNoticia.title!=="" && this.newNoticia.image!=="" &&this.newNoticia.body!=="" && this.newNoticia.date!==this.fecha){
+  guardarNoticia() {
+    if (
+      this.newNoticia.title !== '' &&
+      this.newNoticia.image !== '' &&
+      this.newNoticia.body !== '' &&
+      this.newNoticia.date !== this.fecha
+    ) {
       this.arrayNoticias.push(this.newNoticia);
-    this.newNoticia={
-      title:"",
-      image:"",
-      body:"",
-      date: new Date("1/1/1900")
-     }
-    }else{
-      alert("Es necesario rellenar todos los campos")
+      this.newNoticia = {
+        title: '',
+        image: '',
+        body: '',
+        date: this.fecha,
+      };
+    } else {
+      alert('Es necesario rellenar todos los campos');
     }
-    
-    console.log(this.newNoticia)
+
+    console.log(this.newNoticia);
   }
 }
